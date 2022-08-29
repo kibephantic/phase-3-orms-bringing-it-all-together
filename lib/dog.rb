34ef0f1ce176c1183e090b2a7e22f5103175d4a6
PatrickKibe <phantickibe01@gmail.com>
2022-08-29 23:36:14 +0300

    initial commit

--- lib/dog.rb
+++ lib/dog.rb
@@ -50,11 +50,8 @@ attr_accessor :name, :breed, :id
             # self.new is equivalent to Dog.new
                   self.new(id: row[0], name: row[1], breed: row[2])
                 end
-            
 
 
-
-                
                 def self.all
                   sql = <<-SQL
                     SELECT * FROM dogs
@@ -64,8 +61,6 @@ attr_accessor :name, :breed, :id
                 end
               end
             
-
-
 
 
               def self.find_by_name(name)
@@ -80,8 +75,6 @@ attr_accessor :name, :breed, :id
                 end.first
               end
             
-
-
 
                 def self.find(id)
                     sql = <<-SQL
